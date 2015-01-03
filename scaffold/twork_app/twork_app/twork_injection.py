e04ac44bed8b0ab1c6ce1ff8a94d03bd6adcb3ea
bufferx (ZHANG ZY) <idup2x@gmail.com>
2015-01-03 14:05:59 +0800

    Resetup options in twork_app

--- scaffold/twork_app/twork_app/twork_injection.py
+++ scaffold/twork_app/twork_app/twork_injection.py
@@ -1,4 +1,5 @@
 from twork.utils import gen_logger
+from twork.options import setup_options
 
 from twork_app import APP_NAME, APP_INFO
 from twork_app.web.app import HANDLERS
@@ -9,4 +10,4 @@ def timer_callback():
     gen_logger.debug('time callback')
 
 def setup():
-    pass
+    setup_options()
