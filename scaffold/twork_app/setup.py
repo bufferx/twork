fbf5f7b01567e82739b31849dff559cf9484af58
bufferx (ZHANG ZY) <idup2x@gmail.com>
2015-10-30 01:04:19 +0800

    bumpversion support

--- scaffold/twork_app/setup.py
+++ scaffold/twork_app/setup.py
@@ -30,36 +30,62 @@
 # under the License.
 
 import distutils.core
+import os
 import sys
-# Importing setuptools adds some features like "setup.py develop", but
-# it's optional so swallow the error if it's not there.
-try:
-    import setuptools
-except ImportError:
-    pass
 
-import twork_app
+from twork_app.version import __VERSION__
 
-kwargs = {}
+def _setup():
+    kwargs = {}
 
-major, minor = sys.version_info[:2]
-python_26 = (major > 2 or (major == 2 and minor >= 6))
+    major, minor = sys.version_info[:2]
+    python_26 = (major > 2 or (major == 2 and minor >= 6))
 
-if major >= 3:
-    import setuptools  # setuptools is required for use_2to3
-    kwargs["use_2to3"] = True
+    # Importing setuptools adds some features like "setup.py develop", but
+    # it's optional so swallow the error if it's not there.
+    try:
+        import setuptools
+    except ImportError:
+        pass
 
-distutils.core.setup(
-    name="twork_app",
-    version=twork_app.version,
-    author="Zhang ZY",
-    author_email="idup2x@gmail.com",
-    url="https://github.com/bufferx/twork",
-    license="http://www.apache.org/licenses/LICENSE-2.0",
-    description="twork_app is a twork module which base on tornado",
-    packages = setuptools.find_packages(exclude=["test", "*.log"]),
-    package_data = {
-    },
-    install_requires=['twork'],
-    **kwargs
-)
+    if major >= 3:
+        import setuptools  # setuptools is required for use_2to3
+        kwargs["use_2to3"] = True
+
+    distutils.core.setup(
+        name="twork_app",
+        version=__VERSION__,
+        author="Zhang ZY",
+        author_email="idup2x@gmail.com",
+        url="https://github.com/bufferx/twork",
+        license="http://www.apache.org/licenses/LICENSE-2.0",
+        description="twork_app is a twork module which base on tornado",
+        packages = setuptools.find_packages(exclude=["test", "*.log"]),
+        package_data = {
+        },
+        install_requires=['twork', 'bumpversion==0.5.3'],
+        **kwargs
+    )
+
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'publish':
+            os.system('make publish')
+            sys.exit()
+        elif sys.argv[1] == 'release':
+            if len(sys.argv) < 3:
+                type_ = 'patch'
+            else:
+                type_ = sys.argv[2]
+            assert type_ in ('major', 'minor', 'patch')
+
+            os.system('bumpversion --current-version {} {}'
+                      .format(__VERSION__, type_))
+            sys.exit()
+
+    _setup()
+
+
+if __name__ == '__main__':
+    main()
