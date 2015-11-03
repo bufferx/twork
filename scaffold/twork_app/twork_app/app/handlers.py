3f74810cd84490e40f92220eb65ce84747d2a309
bufferx (ZHANG ZY) <idup2x@gmail.com>
2015-11-03 16:31:07 +0800

    Add comments

--- scaffold/twork_app/twork_app/app/handlers.py
+++ scaffold/twork_app/twork_app/app/handlers.py
@@ -1,10 +1,17 @@
+"""Define URL HANDLERS here
+"""
+
 from twork.web.action import StatHandler
+
 from twork_app.web.action.health_check import HealthCheckHandler
 from twork_app.web.action.not_found import NotFoundHandler
 
 
 HANDLERS = [
-        (r'/v1.0/twork_app/stats$', StatHandler, {'version': (1, 0)}),
-        (r'/healthcheck.html$', HealthCheckHandler, {'version': (1, 0)}),
-        (r"/(.*)$", NotFoundHandler, {'version': (1, 0)}),
+        # stats
+        (r'^/v1.0/twork_app/stats$', StatHandler, {'version': (1, 0)}),
+        # health check
+        (r'^/healthcheck.html$', HealthCheckHandler, {'version': (1, 0)}),
+        # not found
+        (r'^/(.*)$', NotFoundHandler, {'version': (1, 0)}),
         ]
