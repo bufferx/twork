8d04940328b8b88b89e4acf50f8f5d80d899f0b4
bufferx (ZHANG ZY) <idup2x@gmail.com>
2015-11-05 19:17:29 +0800

    Version bumped to v2.0.3

--- twork/__init__.py
+++ twork/__init__.py
@@ -1,6 +1,6 @@
 __import__('pkg_resources').declare_namespace(__name__)
 
-version = "2.0.2"
-version_info = (2, 0, 2)
+version = "2.0.3"
+version_info = (2, 0, 3)
 SERVER_NAME  = 'TWS'
 SERVER_INFO  = '%s/%s' % (SERVER_NAME, version)
