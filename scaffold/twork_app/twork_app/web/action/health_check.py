61c4f77caa892985f647a36714c49ac425ee6cc3
bufferx (ZHANG ZY) <idup2x@gmail.com>
2015-11-10 00:12:51 +0800

    Checking the specified file whether exists when request health_check

--- scaffold/twork_app/twork_app/web/action/health_check.py
+++ scaffold/twork_app/twork_app/web/action/health_check.py
@@ -5,16 +5,25 @@
 '''HealthCheckHandler
 '''
 
+import logging
+
+from tornado.options import options
 from tornado.web import HTTPError
 
+from twork.utils.common import check_file_exists
+
+from twork_app.constants import ST_ITEM_HEALTHCHECK
 from twork_app.web.action.base import BaseHandler
+
+
+gen_logger = logging.getLogger('twork.general')
 
 
 class HealthCheckHandler(BaseHandler):
     '''HealthCheckHandler, RESTFUL SUPPORTED.
     '''
 
-    ST_ITEM = 'HEALTH_CHECK'
+    ST_ITEM = ST_ITEM_HEALTHCHECK
 
     def post(self, *args, **kwargs):
         raise HTTPError(405)
@@ -25,5 +34,16 @@ class HealthCheckHandler(BaseHandler):
     def delete(self, *args, **kwargs):
         raise HTTPError(405)
 
+    def head(self, *args, **kwargs):
+        if not check_file_exists(options.healthcheck_file_path):
+            gen_logger.warn('%s does not exists',
+                    options.healthcheck_file_path)
+            raise HTTPError(404)
+
     def get(self, *args, **kwargs):
+        if not check_file_exists(options.healthcheck_file_path):
+            gen_logger.warn('%s does not exists',
+                    options.healthcheck_file_path)
+            raise HTTPError(404)
+
         self.api_response('It works!')
